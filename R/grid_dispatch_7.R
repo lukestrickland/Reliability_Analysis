50dede111d6f6887faf5643a4e7a53314a6c9d01
Luke Strickland <lukejgstrickland@gmail.com>
2022-09-30 16:49:57 +0800

    random dobbins stuff

---
+++ R/grid_dispatch_7.R
@@ -0,0 +1,9 @@
+rm(list=ls())
+source("dmc/dmc.R")
+
+load_model("LBA", "lba_B.R")
+
+run.grid.dmc("CA_top_strongpriort0svA_samples",model.dir ="LBA",
+             model.file="lba_B.R",user="ljs392",
+             n.add=60, wall.hours = 300,
+             GB = 3, max.try=5)
